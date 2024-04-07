c4dbbeef07fe046bc6b092f388835818a3ecbdbe
scybersec <shimulcybersec@gmail.com>
2024-04-06 21:22:05 -0400

    working with numbers, strings and booleans

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,12 +1,18 @@
-function add (n1: number, n2: number) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('Incorrent input!');
-    }
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //     throw new Error('Incorrent input!');
+  // }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+  return result;
 }
 
 const number1 = 5;
 const number2 = 2.8;
-
-const result = add(number1, number2);
-console.log(result)
+const printResult = true;
+const resultPhrase = "Result is: ";
+add(number1, number2, printResult, resultPhrase);
