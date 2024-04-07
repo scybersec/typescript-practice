95ea2213be9976215f47dd20208e0fddab7f6d13
scybersec <shimulcybersec@gmail.com>
2024-04-06 21:53:35 -0400

    Type Assignment & Type Inference

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -11,8 +11,9 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   return result;
 }
 
-const number1 = 5;
+let number1: number;
+number1 = 5;
 const number2 = 2.8;
 const printResult = true;
-const resultPhrase = "Result is: ";
+let resultPhrase = "Result is: ";
 add(number1, number2, printResult, resultPhrase);
