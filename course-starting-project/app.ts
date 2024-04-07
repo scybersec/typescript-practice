b95f91d8e3e280aec667aae32593ec5cb33bf2e5
scybersec <shimulcybersec@gmail.com>
2024-04-07 10:26:21 -0400

    object types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,19 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //     throw new Error('Incorrent input!');
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-  return result;
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Shimul",
+  age: 30,
+};
 
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-let resultPhrase = "Result is: ";
-add(number1, number2, printResult, resultPhrase);
+console.log(person.age);
