a05676fb49d4f418516017dd00adad35a1183226
scybersec <shimulcybersec@gmail.com>
2024-04-10 20:17:51 -0400

    noImplicitReturns

--- config-00-starting-setup/src/app.ts
+++ config-00-starting-setup/src/app.ts
@@ -1,8 +1,17 @@
 const button = document.querySelector("button")!;
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+}
 function clickHandler(message: string, age: number) {
   let username = "Shimul";
   console.log("Clicked! " + message);
 }
 if (button) {
-  button.addEventListener("click", clickHandler.bind(null, "You're Welcome", 30));
+  button.addEventListener(
+    "click",
+    clickHandler.bind(null, "You're Welcome", 30)
+  );
 }
