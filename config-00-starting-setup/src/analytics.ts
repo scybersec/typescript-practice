f141b45b5690993bc03c07d717cd120b42dd5572
scybersec <shimulcybersec@gmail.com>
2024-04-10 20:13:13 -0400

    strict compilation

--- config-00-starting-setup/src/analytics.ts
+++ config-00-starting-setup/src/analytics.ts
@@ -1,1 +1,10 @@
-console.log('sending....')
+let logged;
+
+function sendAnalytics(data: string) {
+  console.log(data);
+  logged = true;
+  console.log(logged);
+  
+}
+
+sendAnalytics("data");
--- config-00-starting-setup/src/app.ts
+++ config-00-starting-setup/src/app.ts
@@ -1,5 +1,7 @@
-const button = document.querySelector("button");
-
-button.addEventListener("click", () => {
-  console.log("Clicked!");
-});
+const button = document.querySelector("button")!;
+function clickHandler(message: string) {
+  console.log("Clicked! " + message);
+}
+if (button) {
+  button.addEventListener("click", clickHandler.bind(null, "You're Welcome"));
+}
