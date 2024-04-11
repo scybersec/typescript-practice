fc12aadc51c28dcec33c6339230f1d2cf9a645bf
scybersec <shimulcybersec@gmail.com>
2024-04-10 20:19:52 -0400

    Code Quality Options done

--- config-00-starting-setup/src/app.ts
+++ config-00-starting-setup/src/app.ts
@@ -4,6 +4,7 @@ function add(n1: number, n2: number) {
   if (n1 + n2 > 0) {
     return n1 + n2;
   }
+  // return;
 }
 function clickHandler(message: string, age: number) {
   let username = "Shimul";
