c248c11a34071b860413c3c3adbcf5b36edc809a
scybersec <shimulcybersec@gmail.com>
2024-04-10 20:16:16 -0400

    noUnusedParameters

--- config-00-starting-setup/src/app.ts
+++ config-00-starting-setup/src/app.ts
@@ -1,8 +1,8 @@
 const button = document.querySelector("button")!;
-function clickHandler(message: string) {
+function clickHandler(message: string, age: number) {
   let username = "Shimul";
   console.log("Clicked! " + message);
 }
 if (button) {
-  button.addEventListener("click", clickHandler.bind(null, "You're Welcome"));
+  button.addEventListener("click", clickHandler.bind(null, "You're Welcome", 30));
 }
