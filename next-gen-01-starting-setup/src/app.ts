643c29725dfcd4609c8df55034a66a454a2af241
scybersec <shimulcybersec@gmail.com>
2024-04-21 20:56:24 -0400

    let and const

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,16 @@
 // Code goes here!
+const userName = "Max";
+
+let age = 30;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+if (age > 20) {
+  let isOld = true;
+}
+
+console.log(isOld);
