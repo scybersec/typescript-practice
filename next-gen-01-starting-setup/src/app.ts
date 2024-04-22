3729718249e4b6bfca837c281cfdbc5d40ffb6a9
scybersec <shimulcybersec@gmail.com>
2024-04-22 19:29:41 -0400

    the spread operator

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -15,15 +15,32 @@ let age = 30;
 
 // console.log(isOld);
 
-const add = (a: number = 2 , b: number = 3) => a + b;
+const add = (a: number, b: number = 3) => a + b;
 
 const printOutput: (a: number | string) => void = (output) =>
   console.log(output);
 
-printOutput(add());
-
 const button = document.querySelector("button");
 
 if (button) {
-  button.addEventListener("click", event => console.log(event));
+  button.addEventListener("click", (event) => console.log(event));
 }
+
+printOutput(add(5));
+
+const hobbies = ["Sports", "Cooking"];
+
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+console.log(activeHobbies);
+
+const person = {
+  name: "Shimul",
+  age: 24,
+};
+
+const copiedPerson = {...person};
+
+console.log(copiedPerson);
