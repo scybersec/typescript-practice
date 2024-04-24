59766e71484b0923f2d90f7143e75bec550a2950
scybersec <shimulcybersec@gmail.com>
2024-04-23 21:58:32 -0400

    Constructor Functions & The "this" Keyword

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -2,10 +2,18 @@ class Department {
   name: string;
 
   constructor(n: string) {
-    this.name = n;
+    this.name = n
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
   }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department("Accounting");
 
-console.log(accounting);
+accounting.describe();
+
+const accountingCopy = { name: 'Accounting 2', describe: accounting.describe };
+
+accountingCopy.describe();
