917f2201a586ea20d4173fe3b26a59da6c5cda5b
scybersec <shimulcybersec@gmail.com>
2024-04-27 11:12:44 -0400

    public and private keywords in typescript

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,19 +1,34 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
-    this.name = n
+    this.name = n;
   }
 
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
 accounting.describe();
 
-const accountingCopy = { name: 'Accounting 2', describe: accounting.describe };
+accounting.printEmployeeInformation();
 
-accountingCopy.describe();
+// const accountingCopy = { name: "Accounting 2", describe: accounting.describe };
+
+// accountingCopy.describe();
