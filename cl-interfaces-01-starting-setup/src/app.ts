fdf9769dad95e89d78fbce7b0ceca50d21c0b941
scybersec <shimulcybersec@gmail.com>
2024-05-01 20:17:04 -0400

    Static Methods & Properties

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,4 +1,5 @@
 class Department {
+  static fiscalYear = 2024;
   // private id: string;
   // private name: string;
   protected employees: string[] = [];
@@ -6,6 +7,11 @@ class Department {
   constructor(public readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
+    console.log(Department.fiscalYear);
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   describe(this: Department) {
@@ -68,6 +74,9 @@ class AccountingDepartment extends Department {
     console.log(this.reports);
   }
 }
+
+const employee1 = Department.createEmployee("Shimul");
+console.log(employee1, Department.fiscalYear);
 
 const it = new ITDepartment("d2", ["shimul"]);
 
